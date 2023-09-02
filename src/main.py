51f752aaad59efe49c372dc4b08905df3a7fc19a
KAN-YA, Yukitoshi <tenagusami@hotmail.com>
2023-09-02 13:30:12 +0900

    feat: first commit

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 
 import asyncio
 
+from src.modules.chapter01.chapter01 import chapter01
+
 
 async def main() -> None:
     """
@@ -13,7 +15,7 @@ async def main() -> None:
     # prefix_directory: Path = Path(__file__).parent.parent
     # data_directory: Path = prefix_directory / "data"
     try:
-        chaoter01()
+        chapter01()
 
     except KeyboardInterrupt:
         exit(1)
