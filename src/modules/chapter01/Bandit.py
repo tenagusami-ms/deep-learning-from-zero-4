9a1f06fc2e66f3bdb28a11350cb6570c74a1676c
KAN-YA, Yukitoshi <tenagusami@hotmail.com>
2023-09-23 17:32:01 +0900

    feat: chapter 01 is being implemented.

--- src/modules/chapter01/Bandit.py
+++ src/modules/chapter01/Bandit.py
@@ -3,8 +3,9 @@ bandit problem
 """
 from __future__ import annotations
 
+from typing import cast
+
 import numpy as np
-from numpy.typing import NDArray
 
 
 class Bandit:
@@ -13,19 +14,27 @@ class Bandit:
     """
 
     def __init__(self, arms: int = 10):
-        self.rates: NDArray[np.float64] = np.random.rand(arms)
+        self.rates: np.ndarray = np.random.rand(arms)
 
     def play(self, arm: int) -> float:
         """
         play
         """
-        rate: np.float64 = self.rates[arm]
-        print(np.isscalar(rate))
+        rate: np.float64 = cast(np.float64, self.rates[arm])
         return 1.0 if rate > np.random.rand() else 0.0
 
-    def a(self):
-        """
-        a
-        """
-        b: NDArray[np.int32] = np.array([1, 2, 3])
-        b0: np.int32 = b[0]
+
+class Agent:
+    def __init__(self, epsilon: float, action_size: int = 10):
+        self.epsilon: float = epsilon
+        self.Qs: np.ndarray = np.zeros(action_size)
+        self.ns: np.ndarray = np.zeros(action_size)
+
+    def update(self, action: int, reward: float) -> None:
+        self.ns[action] += 1
+        self.Qs[action] += (reward - self.Qs[action]) / self.ns[action]
+
+    def get_action(self) -> int:
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
--- src/modules/chapter01/chapter01.py
+++ src/modules/chapter01/chapter01.py
@@ -3,14 +3,41 @@ chapter01
 """
 from __future__ import annotations
 
-from src.modules.chapter01.Bandit import Bandit
+from typing import MutableSequence
+
+import matplotlib.pyplot as plt
+
+from src.modules.chapter01.Bandit import Bandit, Agent
 
 
 def chapter01() -> None:
     """
     chapter01
     """
-    bandit: Bandit = Bandit()
+    steps: int = 1000
+    epsilon: float = 0.1
 
-    for i in range(3):
-        print(bandit.play(0))
+    bandit: Bandit = Bandit()
+    agent: Agent = Agent(epsilon)
+    total_reward: float = 0.0
+    total_rewards: MutableSequence[float] = []
+    rates: MutableSequence[float] = []
+
+    for step in range(steps):
+        action: int = agent.get_action()
+        reward: float = bandit.play(action)
+        agent.update(action, reward)
+        total_reward += reward
+        total_rewards.append(total_reward)
+        rates.append(total_reward / (step + 1))
+
+    print(total_reward)
+    plt.ylabel("Total reward")
+    plt.xlabel("Steps")
+    plt.plot(total_rewards)
+    plt.show()
+
+    plt.ylabel("Rates")
+    plt.xlabel("Steps")
+    plt.plot(rates)
+    plt.show()
